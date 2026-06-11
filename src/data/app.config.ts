63e128ad8ffe71a8c6a8bf926aa1929d784c11ec
AlexAaqil <alexaaqil.se@gmail.com>
2026-06-11 18:14:49 +0300

    brand strategy updates.

--- src/data/app.config.ts
+++ src/data/app.config.ts
@@ -1,10 +1,31 @@
 import businessOwner from '../assets/images/business-owner.jpeg';
 import womanFounder from '../assets/images/woman-founder.jpeg';
 
+import geniuseeLogo from '../assets/images/competitor-profiles/geniusee/logo.png';
+import geniuseeWebsite from '../assets/images/competitor-profiles/geniusee/webpage.png';
+import questLogo from '../assets/images/competitor-profiles/quest/logo.png';
+import questWebsite from '../assets/images/competitor-profiles/quest/webpage.png';
+import simformLogo from '../assets/images/competitor-profiles/simform/logo.png';
+import simformHero from '../assets/images/competitor-profiles/simform/hero.png';
+import simformServices from '../assets/images/competitor-profiles/simform/services.png';
+import smartCodeWebsite01 from '../assets/images/competitor-profiles/smart-code/webpage-1.png';
+import smartCodeWebsite02 from '../assets/images/competitor-profiles/smart-code/webpage-2.png';
+import smartCodeWebsite03 from '../assets/images/competitor-profiles/smart-code/webpage-3-contact-page.png';
+
 export const AppConfig = {
-  brand_name: 'Aswa',
+  brand_name: 'Alsuwa',
   images: {
     business_owner: businessOwner,
-    woman_founder: womanFounder
+    woman_founder: womanFounder,
+    geniusee_logo: geniuseeLogo,
+    geniusee_website: geniuseeWebsite,
+    quest_logo: questLogo,
+    quest_website: questWebsite,
+    simform_logo: simformLogo,
+    simform_hero: simformHero,
+    simform_services: simformServices,
+    smart_code_website_01: smartCodeWebsite01,
+    smart_code_website_02: smartCodeWebsite02,
+    smart_code_website_03: smartCodeWebsite03
   }
 } as const;
